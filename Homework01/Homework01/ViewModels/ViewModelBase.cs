62d6082ee451f3b743985f5ea17bb16e9981a946
GaboMendez <1071470@est.intec.edu.do>
2020-02-07 15:02:55 -0400

    Updating branch

---
+++ Homework01/Homework01/ViewModels/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Services;
+
+namespace Homework01.ViewModels
+{
+    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
+    {
+        protected INavigationService NavigationService { get; private set; }
+        protected IPageDialogService DialogService { get; private set; }
+
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ViewModelBase(INavigationService navigationService, IPageDialogService pageDialogService)
+        {
+            NavigationService = navigationService;
+            DialogService = pageDialogService;
+        }
+
+        public virtual void Initialize(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
