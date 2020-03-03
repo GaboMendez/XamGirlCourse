70f3fa37e11403b319c3a55b01991b993f0f01a0
GaboSchool <1071470@est.intec.edu.do>
2020-03-03 17:02:41 -0400

    Adding App Resources for English and Spanish

--- Homework03/Homework03.Android/MainActivity.cs
+++ Homework03/Homework03.Android/MainActivity.cs
@@ -15,6 +15,7 @@ namespace Homework03.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
